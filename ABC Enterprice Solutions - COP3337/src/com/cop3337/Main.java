62ca893cb1572ee564b445b4a669f21587f8b146
Diego Matheus <diegoamatheus@gmail.com>
2022-08-29 22:35:59 -0400

    Added option 1, kinda

--- ABC Enterprice Solutions - COP3337/src/com/cop3337/Main.java
+++ ABC Enterprice Solutions - COP3337/src/com/cop3337/Main.java
@@ -10,14 +10,39 @@ public class Main {
     public static void main(String[] args) {
         Menu.Initialize();
 
-        int option = Menu.getMenuOption();
+        int option = Menu.getOption();
         while (option != 7){
             switch (option){
                 case 1:
-                    JOptionPane.showMessageDialog(null, "Something happening here");
+                    int units = 0;
+                    double price = 0.0;
+                    String name;
+                    // Write logic to validate input
+                    name = JOptionPane.showInputDialog(null, "Enter the name of the new product: ");
+                    units = Integer.parseInt(JOptionPane.showInputDialog(null, "How many units? "));
+                    price = Double.parseDouble(JOptionPane.showInputDialog(null, "Price per unit: "));
+                    Product p = new Product(new Manufacturer("Stihl", "1495 NW 24th Avenue"), name, units, price);
+                    JOptionPane.showMessageDialog(null, "This is the product created: " + p.toString());
                     break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "You have pressed option: " + option);
+                break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "You have pressed option: " + option);
+                break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "You have pressed option: " + option);
+                break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "You have pressed option: " + option);
+                break;
+                case 6:
+                    JOptionPane.showMessageDialog(null, "You have pressed option: " + option);
+                break;
+                default:
+                break;    
             }
-            option = Menu.getMenuOption();
+            option = Menu.getOption();
         }
     }
 }
--- ABC Enterprice Solutions - COP3337/src/com/cop3337/Menu.java
+++ ABC Enterprice Solutions - COP3337/src/com/cop3337/Menu.java
@@ -5,24 +5,45 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class Menu {
-    private static String[] options = new String[]{"Create new Product", "View Product", "Update Product", "Delete Product", "Register Sale", "Update Ordered Products", "Exit"};
-    private static String header = "FIU International Bank";
+    private static String[] options = new String[] { "Create new Product",
+            "View Product",
+            "Update Product",
+            "Delete Product",
+            "Register Sale",
+            "Update Ordered Products",
+            "Exit" };
+    private static String header = "FIU International Bank "
+            + DateFormat.getDateInstance(DateFormat.LONG).format(new Date());
     private static StringBuilder menu = new StringBuilder();
 
-    static void Initialize(){
-        if(menu.length() < 1){
-            menu.append(header).append(" ").append(DateFormat.getDateInstance(DateFormat.LONG).format(new Date()).toString());
+    /**
+     * {@summary} Joins the options array and the header into a full string to be
+     * passed to the JOptionPane component.
+     */
+    static void Initialize() {
+        if (menu.length() < 1) {
+            menu.append(header);
             int index = 0;
-            // Loop through the options to type them in the menu
-            for(String option: options) {
+
+            for (String option : options) {
                 ++index;
-                menu.append("\n").append(index).append(")").append(option);
-                
+                menu
+                        .append("\n")
+                        .append(index)
+                        .append(")")
+                        .append(option);
+
             }
         }
     }
 
-    static int getMenuOption(){
+    /**
+     * Parses the input received from the main menu and returns the numeric value of
+     * the option.
+     * 
+     * @return Integer
+     */
+    static int getOption() {
         return Integer.parseInt(JOptionPane.showInputDialog(menu));
     }
 }
