465905c76426d4f214bb979dc37afc10eba78704
Diego Matheus <diegoamatheus@gmail.com>
2022-11-09 20:20:31 -0500

    Added example to tokenize input file via path

--- Assignment4/src/com/cop3337/Validation.java
+++ Assignment4/src/com/cop3337/Validation.java
@@ -7,6 +7,19 @@ public class Validation {
     private HashMap<String, Boolean> symbols;
 
     public static void parseFile(File file) {
+        // final var lines = Files.readAllLines(Path.of("myfile.txt" || file?));
+        // FileWriter writer = new FileWriter( "output.txt");
+        // String data = " ";
+
+        // for (int i = 0; i < lines.size(); i++) {
+        // data = lines.get(i);
+        // StringTokenizer token = new StringTokenizer(data);
+        // while (token.hasMoreElements()) {
+        // writer.write(token.nextToken() + "\n");
+        // }
+        // }
+        // writer.close();
+        // }
         System.out.println("parsingFile...");
     }
 }
